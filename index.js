aaff328c727749c5da406776dc9b82ac2974cf06
JackYang- <jackyanguw@gmail.com>
2014-11-10 00:21:24 -0500

    Separated js/style files and supporting configs.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var fs = require('fs');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/stuff'));
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 //***************************************************************************************************************************
@@ -14,10 +16,11 @@ app.get('/', function(req, res){
 //apply settings
 var settings = require("./settings.json");
 var _listenPort = settings.port ? settings.port : '3000';
-var _listenIP = settings.ip ? settings.ip : '192.168.0.100';
+var _listenIP = settings.ip ? settings.ip : 'asdf';
+var _playersFile = settings.playersFilePath ? settings.playersFilePath : './registeredPlayers.json';
 
 //loading players
-var currentUserData = require("./registeredPlayers.json");
+var currentUserData = require(_playersFile);
 var players = null;
 if (currentUserData)
 {
@@ -25,6 +28,7 @@ if (currentUserData)
 	for (var i in players)
 	{
 		players[i].connected = false;
+		players[i].ingame = false;
 		console.log('checking loaded players: ' + players[i].name + ' ' + players[i].ip);
 	}
 }
@@ -32,7 +36,6 @@ else
 {
 	players = {};
 }
-
 
 //regularly save current registered players
 setInterval(function () {
@@ -47,6 +50,13 @@ setInterval(function () {
 		}});
 }, 60000);
 
+//card game
+var FiveTenKing = function (players, deckCount) 
+{
+	console.log("New Five Ten King instance created.");
+};
+
+//handling socket requests
 io.on('connection', function(socket){
 	var playerIP = socket.handshake.address;
 	var playerSearchResult = getPlayer(playerIP);
@@ -57,9 +67,9 @@ io.on('connection', function(socket){
 		{
 			playerSearchResult.connected = true;
 			console.log(playerSearchResult.name + '(' + playerIP + ')' + ' has connected');
-			socket.emit('set-name', playerSearchResult.name + ' <span class="caret"></a>');
+			socket.emit('set-name', playerSearchResult.name);
 			socket.emit('log-message', 'Welcome back, ' + playerSearchResult.name + '.');
-			socket.emit('log-message', 'Currently, ' + players.length + ' player(s) are online');
+			socket.emit('log-message', 'Currently, ' + getOnlinePlayersCount() + ' player(s) are online');
 		}
 	}
 	else //if player is not registered, then it's a new player
@@ -77,20 +87,35 @@ io.on('connection', function(socket){
 	});
 	socket.on('set-new-name', function(newName) {
 		console.log(playerIP + ' namechange: from ' + playerSearchResult.name + ' to ' + newName);
-		if (setPlayerAttributes(playerIP, "name", newName))
+		if (newName === "UnamedPlayer" || newName === "")
+		{
+			console.log('trying to change name to unamed');
+			socket.emit('log-message', 'Come on bro.');
+		}
+		else if (playerSearchResult.ingame)
+		{
+			console.log('trying to change name in game');
+			socket.emit('log-message', 'Cannot change name while in game!');
+		}
+		else if (playerAttributeExists("name", newName))
+		{
+			console.log('name is in use');
+			socket.emit('log-message', 'This name is already in use.');
+		}
+		else if (setPlayerAttributes(playerIP, "name", newName))
 		{
 			console.log('player name change succeeded');
-			socket.emit('set-name', escapeHtml(newName) + ' <span class="caret"></a>');
+			socket.emit('set-name', escapeHtml(newName));
+			socket.emit('log-message', 'Name change successful.');
 		}
 		else
 		{
-			console.log('player name change succeeded');
+			console.log('player name change failed');
 			socket.emit('log-message', 'An error occurred while changing your name.');
 		}
 		
 	});
 });
-
 
 //***************************************************************************************************************************
 // Helpers
@@ -107,13 +132,25 @@ function escapeHtml(text) {
   return text.replace(/[&<>"']/g, function(m) { return map[m]; });
 }
 
+function playerAttributeExists(attribute, value)
+{
+	for (var i in players)
+	{
+		if (attribute === "name" && players[i].name === value)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
 function setPlayerAttributes (ip, attribute, value)
 {
 	for (var i in players)
 	{
 		if (players[i].ip === ip)
 		{
-			if (attribute === "name")
+			if (attribute === "name" && !(value ==="UnamedPlayer"))
 			{
 				players[i].name = value;
 			}
@@ -125,6 +162,19 @@ function setPlayerAttributes (ip, attribute, value)
 		}
 	}
 	return false;
+}
+
+function getOnlinePlayersCount()
+{
+	var count = 0;
+	for (var i in players)
+	{
+		if (players[i].connected)
+		{
+			count ++;
+		}
+	}
+	return count;
 }
 
 function getPlayer (ip)
---
+++ stuff/scripts.js
@@ -0,0 +1,22 @@
+var socket = io();
+$(document).ready(function () {
+$('form').submit(function(){
+	socket.emit('chat message', $('#m').val());
+	$('#m').val('');
+	return false;
+});
+
+socket.on('log-message', function(msg){
+	$('#messages').append($('<li>').text(msg));
+});
+
+socket.on('set-name', function(msg) {
+	$('#player-dropdown').html(msg);
+});
+
+$('#pf-save-button').click(function () {
+	var newName = $('#pf-new-name').val();
+	socket.emit('set-new-name', newName);
+	$('#myModal').modal('toggle');
+});
+});
